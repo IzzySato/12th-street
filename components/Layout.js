c27e3456f10c99044aff9d4f39880cae3a933b6f
Izzy Sato <izzysato@Izzys-MacBook-Pro-3.local>
2022-10-21 10:20:32 -0700

    added new design

---
+++ components/Layout.js
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import useSWR from 'swr';
+import Nav from './Nav';
+import Footer from './Footer';
+import ContactHeader from './ContactHeader';
+
+const Layout = ({ children }) => {
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const url = '/api/businessInfo';
+  const { data, error } = useSWR(url, fetcher);
+  if (error) return <div>Failed to load.</div>
+  if (!data) return <div>Loading...</div>
+
+  return (
+    <>
+      <Head>
+        <meta
+          name="description"
+          content={data.businessInfo[0].businessDescription} />
+        <title>{data.businessInfo[0].businessName}</title>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+      </Head>
+      <ContactHeader data={{ businessInfo: data.businessInfo[0] }} />
+      <Nav />
+      <div>
+        <main>
+          {children}
+        </main>
+        <Footer data={{ businessInfo: data.businessInfo[0] }} />
+      </div>
+    </>
+  )
+};
+
+export default Layout;
